97f51d807924ab22532e0c047b3af657d470bec7
Samuel Skean <sskean2@uic.edu>
2025-04-06 18:08:37 -0500

    Listening for a connection.

---
+++ war-server-rs/src/main.rs
@@ -0,0 +1,22 @@
+use std::net::IpAddr;
+
+use clap::Parser;
+use tokio::net::TcpListener;
+
+#[derive(clap::Parser)]
+struct Args {
+    host: IpAddr,
+    /// Can be set to 0 to request the OS to pick a port.
+    port: u16,
+}
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    // STRETCH: what would it mean to let the user bind to a string (e.g., a DNS
+    // name)? Should I support that?
+    let listener = TcpListener::bind((args.host, args.port)).await.unwrap();
+    println!("Listening on {addr}", addr=listener.local_addr().unwrap());
+    loop {
+
+    }
+}
